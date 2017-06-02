6de6c6e68cd07893add8a78e234ce5d8de19aa60
VitaliyKiser <v.v.2005@bigmir.net>
2017-06-02 17:30:09 +0300

    Therd version

--- src/test/java/TestsClass.java
+++ src/test/java/TestsClass.java
@@ -6,10 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertTrue;
@@ -19,8 +16,8 @@ public class TestsClass {
     private WebDriver driver;
     private Actions actions;
     private WebElement element;
-    private List<WebElement> elements;
-    private Map<String, String> topRated;
+    private List<WebElement> topRated, productDescription, productPrice;
+
 
     @Before
     public void setUp(){
@@ -37,26 +34,36 @@ public class TestsClass {
         driver.findElement(By.xpath("//*[@id='menu_categories_left']/li[1]/h4/a")).click();
         driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         driver.findElement(By.xpath("//*[@id='menu_categories_left']/li[2]/ul/li[1]/a")).click();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        topRated = driver.findElements(By.cssSelector("i[name='prices_active_element_original']"));
+        productDescription = driver.findElements(By.cssSelector(".g-i-tile-i-title.clearfix>a"));
+        productPrice = driver.findElements(By.cssSelector(".g-price-uah"));
 
-        elements = driver.findElements(By.xpath("//*[@id='catalog_goods_block']/div/div/div[1]/div/div[1]/div[2]/div/div/i"));
-        topRated = new HashMap<String, String>();
+        System.out.println("Start to find elements");
+        System.out.println("topRated " + topRated.size());
+        System.out.println("productDescription " + productDescription.size());
+        System.out.println("productPrice " + productPrice.size());
+        System.out.println("Stop to find elements");
 
-        System.out.println(elements.size());
-
-        for (WebElement element: elements){
-            if (element.getAttribute("class").contains("g-tag g-tag-icon-middle-popularity sprite")){
-                System.out.println("element found");
-                topRated.put(driver.findElement(By.xpath("//*[@id='catalog_goods_block']/div/div/div[1]/div/div[1]/div[2]/div[4]/a")).getText(),
-                        driver.findElement(By.xpath("//*[@id='catalog_goods_block']/div/div/div/div/div[1]/div[2]/div[5]/div/div/div")).getText());
+        int counter = 0;
+        for (WebElement element: topRated) {
+            if (element.getAttribute("class").toString().contains("g-tag g-tag-icon-middle-popularity sprite")) {
+                System.out.println("Advertisment");
+                System.out.println(productDescription.get(counter).getText());
+                System.out.println(productPrice.get(counter).getText());
 
             }
+            counter ++;
+        }
 
-        }
-        for (Map.Entry<String, String> entry : topRated.entrySet()) {
-            String key = entry.getKey().toString();
-            String value = entry.getValue();
-            System.out.println("Model: " + key + ", Price: " + value);
-        }
+
+
+
+
+
+
+
+
 
         //driver.findElement(By.xpath("")); //Top rated
         //List list = driver.findElements(By.xpath("//*[@id='rso']/div[2]/div/div/h3/a"));
